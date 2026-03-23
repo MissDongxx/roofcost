b4023753f54e72cace068c7ede8d7e34b575c3f1
xu <912329331@qq.com>
2026-03-23 18:34:29 +0800

    调整logo大小

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,8 +9,8 @@ export const envConfigs: ConfigMap = {
   app_url: process.env.NEXT_PUBLIC_APP_URL ?? 'https://roofcostai.com',
   app_name: process.env.NEXT_PUBLIC_APP_NAME ?? 'RoofCostAI',
   app_description: process.env.NEXT_PUBLIC_APP_DESCRIPTION ?? '',
-  app_logo: process.env.NEXT_PUBLIC_APP_LOGO ?? '/logo.png',
-  app_favicon: process.env.NEXT_PUBLIC_APP_FAVICON ?? '/logo.png',
+  app_logo: process.env.NEXT_PUBLIC_APP_LOGO ?? '/logo.svg',
+  app_favicon: process.env.NEXT_PUBLIC_APP_FAVICON ?? '/logo.svg',
   app_preview_image:
     process.env.NEXT_PUBLIC_APP_PREVIEW_IMAGE ?? '/preview.png',
   theme: process.env.NEXT_PUBLIC_THEME ?? 'default',
